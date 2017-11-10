a2bbf4cdf3764f958cd59d1db456cf70b7f93ecb
psnider30 <pbsnider@gmail.com>
2017-11-10 13:36:44 -0800

    Done.

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -8,7 +8,7 @@ class GithubService
 
   def authenticate!(client_id, client_secret, code)
     response = Faraday.post "https://github.com/login/oauth/access_token",
-        {client_id: client_id, client_secret: client_secret, code: code},
+        {client_id: client_id, client_secret: client_secret, code: code, },
         {'Accept' => 'application/json'}
     access_hash = JSON.parse(response.body)
     @access_token = access_hash["access_token"]
@@ -22,6 +22,7 @@ class GithubService
   end
 
   def get_repos
+
     response = Faraday.get "https://api.github.com/user/repos", {},{'Authorization' => "token #{self.access_token}", 'Accept' => 'application/json'}
     repos_array = JSON.parse(response.body)
     repos_array.map{|repo| GithubRepo.new(repo) }
