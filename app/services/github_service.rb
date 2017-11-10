4bfa2ec14ae37882536191884e9028bfa3111ce5
psnider30 <pbsnider@gmail.com>
2017-11-10 11:54:05 -0800

    Done.

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -15,19 +15,21 @@ class GithubService
   end
 
   def get_username
-    user_response = Faraday.get "https://api.github.com/user", {}, {'Authorization' => "token #{self.access_token}", 'Accept' => 'application/json'}
+    user_response = Faraday.get "https://api.github.com/user", {},
+    {'Authorization' => "token #{self.access_token}", 'Accept' => 'application/json'}
     user_json = JSON.parse(user_response.body)
     user_json["login"]
   end
 
   def get_repos
-    response = Faraday.get "https://api.github.com/user/repos", {}, {'Authorization' => "token #{self.access_token}", 'Accept' => 'application/json'}
+    response = Faraday.get "https://api.github.com/user/repos", {},{'Authorization' => "token #{self.access_token}", 'Accept' => 'application/json'}
     repos_array = JSON.parse(response.body)
     repos_array.map{|repo| GithubRepo.new(repo) }
   end
 
   def create_repo(name)
-    Faraday.post "https://api.github.com/user/repos", {name: name}.to_json, {'Authorization' => "token #{self.access_token}", 'Accept' => 'application/json'}
+    Faraday.post "https://api.github.com/user/repos", {name: name}.to_json,
+    {'Authorization' => "token #{self.access_token}", 'Accept' => 'application/json'}
   end
 
 end
